4b77230f4ba908f51efcd9826abd3eb31ebe16e4
fabioNog <fabio.carmo@computacao.ufla.br>
2020-09-19 19:26:25 -0300

    Ajustando o json de retorno

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -7,11 +7,11 @@ use Illuminate\Http\Request;
 class ProdutosController extends Controller
 {
     public function index(){
-        return response()->json(){[
-            ['id' => 1, 'name' => 'p1'],
-            ['id' => 1, 'name' => 'p2'],
-            ['id' => 1, 'name' => 'p3'],
-            ['id' => 1, 'name' => 'p4'],
-        ]};
+       return response()->json([
+           ['id' => 1,'name'=> 'P1'],
+           ['id' => 1,'name'=> 'P2'],
+           ['id' => 1,'name'=> 'P3'],
+           ['id' => 1,'name'=> 'P4'],
+       ]);
     }
 }
