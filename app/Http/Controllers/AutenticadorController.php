8c3a001e83a485141189635d22bc233344463c69
fabioNog <fabio.carmo@computacao.ufla.br>
2020-09-19 19:04:43 -0300

    Ajustando o login

--- app/Http/Controllers/AutenticadorController.php
+++ app/Http/Controllers/AutenticadorController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 
+use Illuminate\Support\Facades\Auth;
+
 class AutenticadorController extends Controller
 {
     public function register(Request $request){
@@ -27,17 +29,29 @@ class AutenticadorController extends Controller
     }
 
     public function login(Request $request){
-        $request->validate([
-            'email' => 'required|string|email|unique:users',
-            'password' => 'required|string|confirmed'
-        ]);
+         $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ]); 
 
         $credenciais = [
             'email' => $request->email,
             'password' => $request->password
         ];
 
-        Auth::attempt($credenciais);
+        if(!Auth::attempt($credenciais)){
+            return response()->json()([
+                'res' => 'Acesso Negado'
+            ],401);
+        }
+
+        $user = $request->user();
+        $token = $user->createtoken('token de acesso')->accessToken;
+
+        return response()->json([
+            'token' => $token
+        ],200);
+
     }
 
     public function logout(Request $request){
