71835a4cce387031c5426d94573c6336040ebf97
fabioNog <fabio.carmo@computacao.ufla.br>
2020-09-20 16:02:10 -0300

    Redirecionando a componente e adicionando css

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component} from 'react';
 
-function App() {
-    return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Example Component</div>
 
-                        <div className="card-body">I'm an example component!</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+class app extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <h1>Teste</h1>
+        )
+    }
 }
 
-export default App;
-
-if (document.getElementById('app')) {
-    ReactDOM.render(<App />, document.getElementById('app'));
-}
+export default app;
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,33 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<! — csrf token →
-<meta name="csrf-token" content="{{ csrf_token() }}">
-<title>Fabio Nogueira</title>
-<! — styles →
-<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Links Adicionais -->
+    <!-- <link rel="manifest" href="site.webmanifest"> -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/img/favicon.ico')}}">
+
+    <!-- CSS here -->
+	<link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/owl.carousel.min.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/slicknav.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/flaticon.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/animate.min.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/magnific-popup.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/fontawesome-all.min.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/themify-icons.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/slick.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/nice-select.css')}}">
+	<link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Fabio Nogueira</title>
+
+<!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
 </head>
 <body>
 <div id="app"></div>
