c7c113e9bf4a30ad4f1c17b4761f777a44188d1b
fabioNog <fabio.carmo@computacao.ufla.br>
2020-09-19 21:11:49 -0300

    Criando blades para renderizar email de confirmação

---
+++ resources/views/emails/registroconfirmacao.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h4>Seja Bem-Vindo {{$name}}</h4>
+    <p>Você acabou de acessar o sistema utilizando o email: {{$email}}</p>
+
+    <p>Clique no link abaixo para confirmar seu email</p>
+    <a href="{{$link}}">Clique Aqui</a>
+</body>
+</html>
