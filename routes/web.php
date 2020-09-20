9e3b6be6afdbc983bc750d7ff02c63471b8df559
fabioNog <fabio.carmo@computacao.ufla.br>
2020-09-20 13:16:41 -0300

    Adicionando rota para welcome

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+ */
+
+Route::get('/', function(){
+    return view('welcome');
+}); 
